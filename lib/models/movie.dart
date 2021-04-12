dec0d08389f874441399ce0d2586b3d04cd48714
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-04-12 07:53:09 +0700

    Membuat halaman list Populer Movie

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -13,8 +13,8 @@ class Movie {
   Movie.fromJson(Map<String, dynamic> parsedJson) {
     this.id = parsedJson['id'];
     this.title = parsedJson['title'];
-    this.voteAverage = parsedJson['voteAverage'] * 1.0;
+    this.voteAverage = parsedJson['vote_average'] * 1.0;
     this.overview = parsedJson['overview'];
-    this.posterPath = parsedJson['poster_Path'];
+    this.posterPath = parsedJson['poster_path'];
   }
 }
