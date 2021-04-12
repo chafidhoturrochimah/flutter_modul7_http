d7d996de175d361079b2b68123fb81b7503914e7
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-04-12 07:31:07 +0700

    Membuat halaman list Populer Movie

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -8,31 +8,55 @@ class MovieList extends StatefulWidget {
 
 class _MovieListState extends State<MovieList> {
   //menambahkan variabel service
-  String result ="";
+  // String result ="";
+  // HttpService service;
+  //Update inisialisasi variabel pada class movieliststate
+  int moviesCount;
+  List movies;
   HttpService service;
+
+  //Tambahkan method initialize() pada class movieliststate
+  Future initialize() async {
+    movies = [];
+    movies = await service.getPopularMovies();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
 
   //method override init state agar permintaan ke rest api dapat 
   //dilakukan ketika state di inisialisasi.
   @override
   void initState(){
     service = HttpService();
+    //Tambahkan function initialize pada initState 
+    initialize();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    service.getPopularMovies().then((value) => {
-      setState(() {
-        result = value;
-      })
-    });
     return Scaffold(
       appBar: AppBar(
-        title: Text("Popular Movie"),
+        title: Text("Popular Movies"),
       ),
       
-      body: Container(
-        child: Text(result),
+      // listview dan card berdasarkan data dari initialize
+      body: ListView.builder(
+        itemCount : (this.moviesCount == null) ? 0 : this.moviesCount,
+        itemBuilder: (context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              title: Text(movies[position].title),
+              subtitle: Text(
+                'Rating = ' + movies[position].voteAverage.toString(),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
