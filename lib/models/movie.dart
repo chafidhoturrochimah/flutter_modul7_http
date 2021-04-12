1faabed505d5b0db33449ddd5887a6eecc5a0c44
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-04-12 07:20:05 +0700

    Membuat model untuk response http

---
+++ lib/models/movie.dart
@@ -0,0 +1,20 @@
+class Movie {
+  int id;
+  String title;
+  double voteAverage;
+  String overview;
+  String posterPath;
+
+  //KONSTRUKTOR
+  Movie(this.id, this.title, this.voteAverage, this.overview, this.posterPath);
+
+  // function untuk mengkonversi json menjadi response yang 
+  //sesuai dengan class movie.
+  Movie.fromJson(Map<String, dynamic> parsedJson) {
+    this.id = parsedJson['id'];
+    this.title = parsedJson['title'];
+    this.voteAverage = parsedJson['voteAverage'] * 1.0;
+    this.overview = parsedJson['overview'];
+    this.posterPath = parsedJson['poster_Path'];
+  }
+}
--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_modul7_http/models/movie.dart';
 import 'package:http/http.dart' as http;
 
 class HttpService {
@@ -6,14 +8,18 @@ class HttpService {
   final String baseUrl = 'http://api.themoviedb.org/3/movie/popular?api_key=';
 
   //function ini untuk mengambil response dari server themoviedb.org
-  Future<String> getPopularMovies() async {
+  //update 
+  Future<List> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("Sukses");
-      String response = result.body;
-      return response;
+      //update model
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
       return null;
